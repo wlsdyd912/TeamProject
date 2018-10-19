76f7f6cc735121d0ba0af4b54ebf44b6ef865343
Hajin <eowls1210@naver.com>
2018-10-19 17:03:31 +0900

    Button 추가 및 TextView 변환 EditText 추가

--- app/src/main/java/edu/android/temproject/MainActivity.java
+++ app/src/main/java/edu/android/temproject/MainActivity.java
@@ -2,11 +2,15 @@ package edu.android.temproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.btnTest);
+        editText = findViewById(R.id.editText);
+
+    }
+
+    public void ChangeText(View view) {
+    String text = editText.getText().toString();
+    textView.setText(text);
+
     }
 }
